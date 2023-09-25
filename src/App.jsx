929f9c138984afa9d01554633f6d586ba9bc9cac
PhamTrungHieu20204968 <phamhieu15082002@gmail.com>
2023-09-25 16:43:55 +0700

    Task #69132 Trang giá vé

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import './App.css';
 import RequireAuth from './features/auth/components/RequireAuth';
 import Home from './features/home/Home';
 import Auth from './features/auth/Auth';
+import TicketPrice from './features/auth/TicketPrice/TicketPrice';
 
 function App() {
   return (
-    <Routes path="/" element={<Outlet />}>
-      <Route path="/auth" element={<Auth />} />
+    <Routes path='/' element={<Outlet />}>
+      <Route path='/auth' element={<Auth />} />
+      <Route path='/ticket_price' element={<TicketPrice />} />
 
       <Route element={<RequireAuth />}>
-        <Route path="/" element={<Home />}></Route>
+        <Route path='/' element={<Home />}></Route>
       </Route>
     </Routes>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,132 +1,132 @@
-import { DownOutlined, BarsOutlined } from "@ant-design/icons";
-import { Dropdown, Space, Popover } from "antd";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { DownOutlined, BarsOutlined } from '@ant-design/icons';
+import { Dropdown, Space, Popover } from 'antd';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import logo from "../../assets/imgs/logo.png";
-import unitedFlag from "../../assets/imgs/united-kingdom.png";
-import { selectCinema } from "../../features/theater/CinemaSlice";
+import logo from '../../assets/imgs/logo.png';
+import unitedFlag from '../../assets/imgs/united-kingdom.png';
+import { selectCinema } from '../../features/theater/CinemaSlice';
 
 const items = [
   {
-    key: "1",
-    label: "Hà Nội",
+    key: '1',
+    label: 'Hà Nội',
     children: [
       {
-        key: "1-1",
-        label: "Beta Thanh Xuân",
+        key: '1-1',
+        label: 'Beta Thanh Xuân',
       },
       {
-        key: "1-2",
-        label: "Beta Mỹ Đình",
+        key: '1-2',
+        label: 'Beta Mỹ Đình',
       },
       {
-        key: "1-3",
-        label: "Beta Đan Phượng",
+        key: '1-3',
+        label: 'Beta Đan Phượng',
       },
       {
-        key: "1-4",
-        label: "Beta Giải Phóng",
+        key: '1-4',
+        label: 'Beta Giải Phóng',
       },
       {
-        key: "1-5",
-        label: "Beta Merchandise",
+        key: '1-5',
+        label: 'Beta Merchandise',
       },
     ],
   },
   {
-    key: "2",
-    label: "TP Hồ Chí Minh",
+    key: '2',
+    label: 'TP Hồ Chí Minh',
     children: [
       {
-        key: "2-1",
-        label: "Beta Quang Trung",
+        key: '2-1',
+        label: 'Beta Quang Trung',
       },
     ],
   },
   {
-    key: "3",
-    label: "Bắc Giang",
+    key: '3',
+    label: 'Bắc Giang',
     children: [
       {
-        key: "3-1",
-        label: "Beta Bắc Giang",
+        key: '3-1',
+        label: 'Beta Bắc Giang',
       },
     ],
   },
   {
-    key: "4",
-    label: "Đồng Nai",
+    key: '4',
+    label: 'Đồng Nai',
     children: [
       {
-        key: "4-1",
-        label: "Beta Biên Hòa",
+        key: '4-1',
+        label: 'Beta Biên Hòa',
       },
       {
-        key: "4-2",
-        label: "Beta Long Khánh",
+        key: '4-2',
+        label: 'Beta Long Khánh',
       },
       {
-        key: "4-3",
-        label: "Beta Long Thành",
+        key: '4-3',
+        label: 'Beta Long Thành',
       },
     ],
   },
   {
-    key: "5",
-    label: "Khánh Hòa",
+    key: '5',
+    label: 'Khánh Hòa',
     children: [
       {
-        key: "5-1",
-        label: "Beta Nha Trang",
+        key: '5-1',
+        label: 'Beta Nha Trang',
       },
     ],
   },
   {
-    key: "6",
-    label: "Thái Nguyên",
+    key: '6',
+    label: 'Thái Nguyên',
     children: [
       {
-        key: "6-1",
-        label: "Beta Thái Nguyên",
+        key: '6-1',
+        label: 'Beta Thái Nguyên',
       },
     ],
   },
   {
-    key: "7",
-    label: "Thanh Hóa",
+    key: '7',
+    label: 'Thanh Hóa',
     children: [
       {
-        key: "7-1",
-        label: "Beta Thanh Hóa",
+        key: '7-1',
+        label: 'Beta Thanh Hóa',
       },
     ],
   },
   {
-    key: "8",
-    label: "Bà Rịa - Vũng Tàu",
+    key: '8',
+    label: 'Bà Rịa - Vũng Tàu',
     children: [
       {
-        key: "8-1",
-        label: "Beta Phú Mỹ",
+        key: '8-1',
+        label: 'Beta Phú Mỹ',
       },
       {
-        key: "8-2",
-        label: "Beta Hồ Tràm",
+        key: '8-2',
+        label: 'Beta Hồ Tràm',
       },
     ],
   },
   {
-    key: "9",
-    label: "Bình Dương",
+    key: '9',
+    label: 'Bình Dương',
     children: [
       {
-        key: "9-1",
-        label: "Beta Empire Bình Dương",
+        key: '9-1',
+        label: 'Beta Empire Bình Dương',
       },
       {
-        key: "9-2",
-        label: "Beta Tân Uyên",
+        key: '9-2',
+        label: 'Beta Tân Uyên',
       },
     ],
   },
@@ -144,39 +144,39 @@ function Header() {
   };
 
   const navigation = (
-    <ul className="navbar">
-      <li className="h-full font-semibold text-lg px-2 cursor-pointer rounded-sm hover:text-white hover:bg-blue-500">
-        <a className="uppercase hover:text-white" href="#">
+    <ul className='navbar'>
+      <li className='h-full font-semibold text-lg px-2 cursor-pointer rounded-sm hover:text-white hover:bg-blue-500'>
+        <a className='uppercase hover:text-white' href='#'>
           Lịch chiếu theo rạp
         </a>
       </li>
-      <li className="h-full font-semibold text-lg px-2 cursor-pointer rounded-sm hover:text-white hover:bg-blue-500">
-        <a className="uppercase hover:text-white" href="#">
+      <li className='h-full font-semibold text-lg px-2 cursor-pointer rounded-sm hover:text-white hover:bg-blue-500'>
+        <a className='uppercase hover:text-white' href='#'>
           Phim
         </a>
       </li>
-      <li className="h-full font-semibold text-lg px-2 cursor-pointer rounded-sm hover:text-white hover:bg-blue-500">
-        <a className="uppercase hover:text-white" href="#">
+      <li className='h-full font-semibold text-lg px-2 cursor-pointer rounded-sm hover:text-white hover:bg-blue-500'>
+        <a className='uppercase hover:text-white' href='#'>
           Rạp
         </a>
       </li>
-      <li className="h-full font-semibold text-lg px-2 cursor-pointer rounded-sm hover:text-white hover:bg-blue-500">
-        <a className="uppercase hover:text-white" href="#">
+      <li className='h-full font-semibold text-lg px-2 cursor-pointer rounded-sm hover:text-white hover:bg-blue-500'>
+        <a className='uppercase hover:text-white' href='#'>
           Giá Vé
         </a>
       </li>
-      <li className="h-full font-semibold text-lg px-2 cursor-pointer rounded-sm hover:text-white hover:bg-blue-500">
-        <a className="uppercase hover:text-white" href="#">
+      <li className='h-full font-semibold text-lg px-2 cursor-pointer rounded-sm hover:text-white hover:bg-blue-500'>
+        <a className='uppercase hover:text-white' href='#'>
           Tin mới và ưu đãi
         </a>
       </li>
-      <li className="h-full font-semibold text-lg px-2 cursor-pointer rounded-sm hover:text-white hover:bg-blue-500">
-        <a className="uppercase hover:text-white" href="#">
+      <li className='h-full font-semibold text-lg px-2 cursor-pointer rounded-sm hover:text-white hover:bg-blue-500'>
+        <a className='uppercase hover:text-white' href='#'>
           Nhượng quyền
         </a>
       </li>
-      <li className="h-full font-semibold text-lg px-2 cursor-pointer rounded-sm hover:text-white hover:bg-blue-500">
-        <a className="uppercase hover:text-white" href="#">
+      <li className='h-full font-semibold text-lg px-2 cursor-pointer rounded-sm hover:text-white hover:bg-blue-500'>
+        <a className='uppercase hover:text-white' href='#'>
           Thành Viên
         </a>
       </li>
@@ -199,33 +199,33 @@ function Header() {
   }, []);
   return (
     <header>
-      <div className="pre-header w-full bg-black h-6">
-        <div className="max-w-screen-xl h-full mx-auto flex justify-end items-center">
-          <a href="#" className="text-white text-sm hover:underline px-2 ">
+      <div className='pre-header w-full bg-black h-6'>
+        <div className='max-w-screen-xl h-full mx-auto flex justify-end items-center'>
+          <a href='#' className='text-white text-sm hover:underline px-2 '>
             Đăng nhập
           </a>
           <a
-            href="#"
-            className="text-white text-sm hover:underline px-2 border-l-2"
+            href='#'
+            className='text-white text-sm hover:underline px-2 border-l-2'
           >
             Đăng ký
           </a>
-          <a href="#" className="pl-2">
-            <img src={unitedFlag} alt="united flag" />
+          <a href='#' className='pl-2'>
+            <img src={unitedFlag} alt='united flag' />
           </a>
         </div>
       </div>
 
       <div
         className={
-          "header w-full bg-white h-20 shadow z-50" +
-          (fixedHeader ? " fixed top-0" : "")
+          'header w-full bg-white h-20 shadow z-50' +
+          (fixedHeader ? ' fixed top-0' : '')
         }
       >
-        <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between px-8 md:w-full xl:px-0">
-          <img src={logo} alt="logo" />
-          <div className="flex items-center">
-            <div className="dropdown rounded-lg border-2 mr-4">
+        <div className='max-w-screen-xl h-full mx-auto flex items-center justify-between px-8 md:w-full xl:px-0'>
+          <img src={logo} alt='logo' />
+          <div className='flex items-center'>
+            <div className='dropdown rounded-lg border-2 mr-4'>
               <Dropdown
                 menu={{
                   items,
@@ -236,7 +236,7 @@ function Header() {
                   defaultSelectedKeys: "6-1",
                 }}
                 arrow
-                className="px-4"
+                className='px-4'
               >
                 <a
                   onClick={(e) => {
@@ -251,39 +251,39 @@ function Header() {
               </Dropdown>
             </div>
 
-            <ul className="navbar hidden xl:flex">
-              <li className="h-full font-semibold text-lg px-2 hover:text-blue-500">
-                <a className="uppercase" href="#">
+            <ul className='navbar hidden xl:flex'>
+              <li className='h-full font-semibold text-lg px-2 hover:text-blue-500'>
+                <a className='uppercase' href='#'>
                   Lịch chiếu theo rạp
                 </a>
               </li>
-              <li className="h-full font-semibold text-lg px-2 hover:text-blue-500">
-                <a className="uppercase" href="#">
+              <li className='h-full font-semibold text-lg px-2 hover:text-blue-500'>
+                <a className='uppercase' href='#'>
                   Phim
                 </a>
               </li>
-              <li className="h-full font-semibold text-lg px-2 hover:text-blue-500">
-                <a className="uppercase" href="#">
+              <li className='h-full font-semibold text-lg px-2 hover:text-blue-500'>
+                <a className='uppercase' href='#'>
                   Rạp
                 </a>
               </li>
-              <li className="h-full font-semibold text-lg px-2 hover:text-blue-500">
-                <a className="uppercase" href="#">
+              <li className='h-full font-semibold text-lg px-2 hover:text-blue-500'>
+                <a className='uppercase' href='#'>
                   Giá Vé
                 </a>
               </li>
-              <li className="h-full font-semibold text-lg px-2 hover:text-blue-500">
-                <a className="uppercase" href="#">
+              <li className='h-full font-semibold text-lg px-2 hover:text-blue-500'>
+                <a className='uppercase' href='#'>
                   Tin mới và ưu đãi
                 </a>
               </li>
-              <li className="h-full font-semibold text-lg px-2 hover:text-blue-500">
-                <a className="uppercase" href="#">
+              <li className='h-full font-semibold text-lg px-2 hover:text-blue-500'>
+                <a className='uppercase' href='#'>
                   Nhượng quyền
                 </a>
               </li>
-              <li className="h-full font-semibold text-lg px-2 hover:text-blue-500">
-                <a className="uppercase" href="#">
+              <li className='h-full font-semibold text-lg px-2 hover:text-blue-500'>
+                <a className='uppercase' href='#'>
                   Thành Viên
                 </a>
               </li>
@@ -291,11 +291,11 @@ function Header() {
 
             <Popover
               content={navigation}
-              title="Menu"
-              trigger="click"
-              placement="bottomRight"
+              title='Menu'
+              trigger='click'
+              placement='bottomRight'
             >
-              <BarsOutlined className="block xl:hidden text-lg" />
+              <BarsOutlined className='block xl:hidden text-lg' />
             </Popover>
           </div>
         </div>
